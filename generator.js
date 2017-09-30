2e7bf1411455ef930011ca6fe66bd946fde06d07
Michael Tsang <michaeltsang1998@gmail.com>
2017-09-30 23:32:45 +0100

    add submission buttons

--- generator.js
+++ generator.js
@@ -1,11 +1,30 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+context.font = '20px serif';
+context.fillStyle = 'white';
+var imageObj = new Image();
+var nameBox = document.getElementById('nameInput');
+var masterBox = document.getElementById('masterInput');
+
+var namePrefix = "[STAND NAME]";
+var masterPrefix = "[STAND MASTER]";
+
 window.onload = function() {
-  var canvas = document.getElementById('myCanvas');
-  var context = canvas.getContext('2d');
-  var imageObj = new Image();
   imageObj.onload = function() {
-    context.drawImage(imageObj, 69, 50);
-    context.font = '48px serif';
-    context.fillText('Hello world', 200, 200);
+    updateImage();
   };
   imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+}
+
+function updateImage() {
+  context.drawImage(imageObj, 69, 50);
+  var nameText = namePrefix + nameBox.value;
+  var masterText = masterPrefix + masterBox.value;
+  strokeFillText(nameText, 100, 100);
+  strokeFillText(masterText, 200, 200);
+}
+
+function strokeFillText(text, x, y) {
+  context.strokeText(text, x, y);
+  context.fillText(text, x, y);
 }
