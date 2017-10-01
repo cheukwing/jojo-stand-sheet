e31fe983f12622fd50a272a44bdd68644be6df7c
Michael Tsang <michaeltsang1998@gmail.com>
2017-10-01 16:33:53 +0100

    some cleanup

--- js/generator.js
+++ js/generator.js
@@ -1,43 +1,46 @@
 var canvas = document.getElementById('myCanvas');
 var nameBox = document.getElementById('nameInput');
 var masterBox = document.getElementById('masterInput');
+var customImage = document.getElementById('imageUpload');
 
 var namePrefix = "[STAND NAME]";
 var masterPrefix = "[STAND MASTER]";
 
 var imageObj = new Image();
-var context = canvas.getContext('2d');
+imageObj.onload = function() {
+  canvas.height = imageObj.height;
+  canvas.width = imageObj.width;
+  updateText();
+};
+
 var fontSize = 50;
 
 window.onload = function() {
-  imageObj.onload = function() {
-    updateImage();
-  };
-  imageObj.src = 'img/example2.jpeg'
-}
-
-function setContext() {
-  canvas.height = imageObj.height;
-  canvas.width = imageObj.width;
-  context = canvas.getContext('2d');
-  fontSize = 50;
-  context.font = fontSize + "px serif";
-  context.fillStyle = "white";
+  imageObj.src = 'img/example2.jpeg';
 }
 
 function updateImage() {
-  context.textAlign = "start";
-  setContext();
-  context.drawImage(imageObj, 0, 0);
-  // unsure of why fontSize does not result in correct height for stand name
-  strokeFillText(namePrefix, fontSize, fontSize * 1.5);
-  strokeFillText(nameBox.value, fontSize, fontSize * 2.5);
-  context.textAlign = "end";
-  strokeFillText(masterPrefix, canvas.width - fontSize, canvas.height - (fontSize * 2));
-  strokeFillText(masterBox.value, canvas.width - fontSize, canvas.height - fontSize);
+  if (true) {
+    imageObj.src = 'img/example.jpeg';
+  }
+  updateText();
 }
 
-function strokeFillText(text, x, y) {
+function updateText() {
+  var context = canvas.getContext('2d');
+  context.font = fontSize + "px serif";
+  context.fillStyle = "white";
+  context.textAlign = "start";
+  context.drawImage(imageObj, 0, 0);
+  // unsure of why fontSize does not result in correct height for stand name
+  strokeFillText(context, namePrefix, fontSize, fontSize * 1.5);
+  strokeFillText(context, nameBox.value, fontSize, fontSize * 2.5);
+  context.textAlign = "end";
+  strokeFillText(context, masterPrefix, canvas.width - fontSize, canvas.height - (fontSize * 2));
+  strokeFillText(context, masterBox.value, canvas.width - fontSize, canvas.height - fontSize);
+}
+
+function strokeFillText(context, text, x, y) {
   context.strokeText(text, x, y);
   context.fillText(text, x, y);
 }
